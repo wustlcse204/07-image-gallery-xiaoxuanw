5aff2b0e79028fc916733f48f643ef9b7dc4fff5
xiaoxuanwu <xiaoxuanwu@wustl.edu>
2021-03-28 17:09:07 -0400

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//get the enlarged box
+var enlargedBox = document.getElementById("enlarged-box");
+
+//get the image 
+var img = document.getElementById("image-1");
+var boxImage = document.getElementById("boxed-image")
+
+
+function enlarge() {
+    img.style.transform = "scale(2)";
+}
+
+function closeImage() {
+
+}
+
+function changeImage() {
+
+}
